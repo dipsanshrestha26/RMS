bb96ea8144937702c79d34a225b67a55456fc694
Dipsan Shrestha <dipsanshrestha26@gmail.com>
2026-03-07 21:31:28 +0545

    Updated movie module and improved customer UI

--- RMS/Controllers/CustomerController.cs
+++ RMS/Controllers/CustomerController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Oracle.ManagedDataAccess.Client;
 using RMS.Data;
+using System;
 
 namespace RMS.Controllers
 {
@@ -20,47 +21,81 @@ namespace RMS.Controllers
                 FROM CUSTOMER_3NF
                 ORDER BY CUSTOMER_ID
             ");
+
             return View(dt);
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Create(string customerId, string customerName, string customerAddress, string customerPhone)
         {
-            _db.Execute(@"
-                INSERT INTO CUSTOMER_3NF (CUSTOMER_ID, CUSTOMER_NAME, CUSTOMER_ADDRESS, CUSTOMER_PHONE)
-                VALUES (:CUSTOMER_ID, :CUSTOMER_NAME, :CUSTOMER_ADDRESS, :CUSTOMER_PHONE)
-            ",
-            new OracleParameter("CUSTOMER_ID", customerId),
-            new OracleParameter("CUSTOMER_NAME", customerName),
-            new OracleParameter("CUSTOMER_ADDRESS", customerAddress),
-            new OracleParameter("CUSTOMER_PHONE", customerPhone));
+            try
+            {
+                _db.Execute(@"
+                    INSERT INTO CUSTOMER_3NF (CUSTOMER_ID, CUSTOMER_NAME, CUSTOMER_ADDRESS, CUSTOMER_PHONE)
+                    VALUES (:CUSTOMER_ID, :CUSTOMER_NAME, :CUSTOMER_ADDRESS, :CUSTOMER_PHONE)
+                ",
+                new OracleParameter("CUSTOMER_ID", customerId),
+                new OracleParameter("CUSTOMER_NAME", customerName),
+                new OracleParameter("CUSTOMER_ADDRESS", customerAddress),
+                new OracleParameter("CUSTOMER_PHONE", customerPhone));
+
+                TempData["SuccessMessage"] = "Customer added successfully.";
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = "Failed to add customer. " + ex.Message;
+            }
 
             return RedirectToAction("Index");
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Update(string customerId, string customerName, string customerAddress, string customerPhone)
         {
-            _db.Execute(@"
-                UPDATE CUSTOMER_3NF
-                SET CUSTOMER_NAME=:CUSTOMER_NAME,
-                    CUSTOMER_ADDRESS=:CUSTOMER_ADDRESS,
-                    CUSTOMER_PHONE=:CUSTOMER_PHONE
-                WHERE CUSTOMER_ID=:CUSTOMER_ID
-            ",
-            new OracleParameter("CUSTOMER_NAME", customerName),
-            new OracleParameter("CUSTOMER_ADDRESS", customerAddress),
-            new OracleParameter("CUSTOMER_PHONE", customerPhone),
-            new OracleParameter("CUSTOMER_ID", customerId));
+            try
+            {
+                _db.Execute(@"
+                    UPDATE CUSTOMER_3NF
+                    SET CUSTOMER_NAME = :CUSTOMER_NAME,
+                        CUSTOMER_ADDRESS = :CUSTOMER_ADDRESS,
+                        CUSTOMER_PHONE = :CUSTOMER_PHONE
+                    WHERE CUSTOMER_ID = :CUSTOMER_ID
+                ",
+                new OracleParameter("CUSTOMER_NAME", customerName),
+                new OracleParameter("CUSTOMER_ADDRESS", customerAddress),
+                new OracleParameter("CUSTOMER_PHONE", customerPhone),
+                new OracleParameter("CUSTOMER_ID", customerId));
+
+                TempData["SuccessMessage"] = "Customer updated successfully.";
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = "Failed to update customer. " + ex.Message;
+            }
 
             return RedirectToAction("Index");
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Delete(string customerId)
         {
-            _db.Execute("DELETE FROM CUSTOMER_3NF WHERE CUSTOMER_ID=:CUSTOMER_ID",
+            try
+            {
+                _db.Execute(@"
+                    DELETE FROM CUSTOMER_3NF
+                    WHERE CUSTOMER_ID = :CUSTOMER_ID
+                ",
                 new OracleParameter("CUSTOMER_ID", customerId));
+
+                TempData["SuccessMessage"] = "Customer deleted successfully.";
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = "Failed to delete customer. " + ex.Message;
+            }
 
             return RedirectToAction("Index");
         }
--- RMS/Controllers/MovieController.cs
+++ RMS/Controllers/MovieController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Oracle.ManagedDataAccess.Client;
 using RMS.Data;
+using System;
 
 namespace RMS.Controllers
 {
@@ -20,53 +21,89 @@ namespace RMS.Controllers
                 FROM MOVIE_3NF
                 ORDER BY MOVIE_ID
             ");
+
             return View(dt);
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Create(string movieId, string movieTitle, string movieGenre, string movieLanguage, string movieDuration, DateTime releaseDate)
         {
-            _db.Execute(@"
-                INSERT INTO MOVIE_3NF (MOVIE_ID, MOVIE_TITLE, MOVIE_GENRE, MOVIE_LANGUAGE, MOVIE_DURATION, RELEASE_DATE)
-                VALUES (:MOVIE_ID, :MOVIE_TITLE, :MOVIE_GENRE, :MOVIE_LANGUAGE, :MOVIE_DURATION, :RELEASE_DATE)
-            ",
-            new OracleParameter("MOVIE_ID", movieId),
-            new OracleParameter("MOVIE_TITLE", movieTitle),
-            new OracleParameter("MOVIE_GENRE", movieGenre),
-            new OracleParameter("MOVIE_LANGUAGE", movieLanguage),
-            new OracleParameter("MOVIE_DURATION", movieDuration),
-            new OracleParameter("RELEASE_DATE", releaseDate));
+            try
+            {
+                _db.Execute(@"
+                    INSERT INTO MOVIE_3NF 
+                    (MOVIE_ID, MOVIE_TITLE, MOVIE_GENRE, MOVIE_LANGUAGE, MOVIE_DURATION, RELEASE_DATE)
+                    VALUES 
+                    (:MOVIE_ID, :MOVIE_TITLE, :MOVIE_GENRE, :MOVIE_LANGUAGE, :MOVIE_DURATION, :RELEASE_DATE)
+                ",
+                new OracleParameter("MOVIE_ID", movieId),
+                new OracleParameter("MOVIE_TITLE", movieTitle),
+                new OracleParameter("MOVIE_GENRE", movieGenre),
+                new OracleParameter("MOVIE_LANGUAGE", movieLanguage),
+                new OracleParameter("MOVIE_DURATION", movieDuration),
+                new OracleParameter("RELEASE_DATE", releaseDate));
+
+                TempData["SuccessMessage"] = "Movie added successfully.";
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = "Failed to add movie. " + ex.Message;
+            }
 
             return RedirectToAction("Index");
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Update(string movieId, string movieTitle, string movieGenre, string movieLanguage, string movieDuration, DateTime releaseDate)
         {
-            _db.Execute(@"
-                UPDATE MOVIE_3NF
-                SET MOVIE_TITLE=:MOVIE_TITLE,
-                    MOVIE_GENRE=:MOVIE_GENRE,
-                    MOVIE_LANGUAGE=:MOVIE_LANGUAGE,
-                    MOVIE_DURATION=:MOVIE_DURATION,
-                    RELEASE_DATE=:RELEASE_DATE
-                WHERE MOVIE_ID=:MOVIE_ID
-            ",
-            new OracleParameter("MOVIE_TITLE", movieTitle),
-            new OracleParameter("MOVIE_GENRE", movieGenre),
-            new OracleParameter("MOVIE_LANGUAGE", movieLanguage),
-            new OracleParameter("MOVIE_DURATION", movieDuration),
-            new OracleParameter("RELEASE_DATE", releaseDate),
-            new OracleParameter("MOVIE_ID", movieId));
+            try
+            {
+                _db.Execute(@"
+                    UPDATE MOVIE_3NF
+                    SET MOVIE_TITLE = :MOVIE_TITLE,
+                        MOVIE_GENRE = :MOVIE_GENRE,
+                        MOVIE_LANGUAGE = :MOVIE_LANGUAGE,
+                        MOVIE_DURATION = :MOVIE_DURATION,
+                        RELEASE_DATE = :RELEASE_DATE
+                    WHERE MOVIE_ID = :MOVIE_ID
+                ",
+                new OracleParameter("MOVIE_TITLE", movieTitle),
+                new OracleParameter("MOVIE_GENRE", movieGenre),
+                new OracleParameter("MOVIE_LANGUAGE", movieLanguage),
+                new OracleParameter("MOVIE_DURATION", movieDuration),
+                new OracleParameter("RELEASE_DATE", releaseDate),
+                new OracleParameter("MOVIE_ID", movieId));
+
+                TempData["SuccessMessage"] = "Movie updated successfully.";
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = "Failed to update movie. " + ex.Message;
+            }
 
             return RedirectToAction("Index");
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Delete(string movieId)
         {
-            _db.Execute("DELETE FROM MOVIE_3NF WHERE MOVIE_ID=:MOVIE_ID",
+            try
+            {
+                _db.Execute(@"
+                    DELETE FROM MOVIE_3NF
+                    WHERE MOVIE_ID = :MOVIE_ID
+                ",
                 new OracleParameter("MOVIE_ID", movieId));
+
+                TempData["SuccessMessage"] = "Movie deleted successfully.";
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = "Failed to delete movie. " + ex.Message;
+            }
 
             return RedirectToAction("Index");
         }
